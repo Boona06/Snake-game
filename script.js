7b9d9cdf6015a9d64540ee9b707b8cfdea260cc6
Boona06 <bb851913@gmail.com>
2024-11-19 00:45:17 +0800

    snake

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const snakeEl = document.getElementById("Snake");
-function goUp() {}
+let headtop = 40 ;
+let headleft = 20 ;
+let direction = "up" ;
+function render(){
+    let snakeHtml = `<div id="Snake" class="Snake" style="top: ${headtop}px; left:${headleft}px"></div>`
+    const boardel = document.getElementById("board")
+    boardel.innerHTML = snakeHtml
+}
+function goUp() {
+    headtop= headtop - 20
+    render()
+    }
+setInterval(goUp , 1000)
+render()
